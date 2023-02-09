b850161ad2d31e33bc68cd4d8263b5320c3a7f28
이상윤 <sangyun.lee@cmsedu.co.kr>
2023-02-09 14:38:43 +0900

    [Init] Acmicpc Java

---
+++ acmicpc/java/practise_algorithem/src/main/java/com/example/practise_algorithem/PractiseAlgorithemApplication.java
@@ -0,0 +1,12 @@
+package com.example.practise_algorithem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseAlgorithemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PractiseAlgorithemApplication.class, args);
+    }
+
+}
---
+++ acmicpc/java/practise_algorithem/src/test/java/com/example/practise_algorithem/PractiseAlgorithemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practise_algorithem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseAlgorithemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
